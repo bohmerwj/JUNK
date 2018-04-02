3324cdb9436556ada9ab2db22cc4f1ab42a4b55e
William Bohmer <bohmerwj@gmail.com>
2018-04-02 19:57:38 -0400

    BASE

---
+++ JUNK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JUNK
+//
+//  Created by duser on 4/2/18.
+//  Copyright © 2018 William Bohmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
