e8c2db83cf953cec925aa37bb98a8442716cc338
William Bohmer <bohmerwj@gmail.com>
2018-04-02 21:08:38 -0400

    override 2 deleted

--- JUNK/ViewController.swift
+++ JUNK/ViewController.swift
@@ -11,11 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+ 
 
 }
 
